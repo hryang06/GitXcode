3cd6818137858adff5cbe4295a92505cd20f22f4
Hee-Rin Yang <helengmlfls@gmail.com>
2020-05-05 22:19:38 +0900

    Initial Commit

---
+++ GitXcode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitXcode
+//
+//  Created by hryang on 2020/05/05.
+//  Copyright © 2020 Hee-Rin Yang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
